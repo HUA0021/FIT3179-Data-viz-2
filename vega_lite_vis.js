6a520b7ab52d575a4a8fe6f69203c177947e7baf
HUA0021 <qq84754080@gmail.com>
2025-10-20 17:52:27 +1100

    asd

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,5 +1,23 @@
+// Define your visualization JSON files
 var vg_1 = "bird_map_code.json";
-vegaEmbed("#bird_map", vg_1).then(function(result) {
-// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+var vg_2 = "bird_vs_rain.json";
+var vg_3 = "bird_over_time.json";
 
-}).catch(console.error);
+// Embed all three Vega-Lite visualizations
+vegaEmbed("#bird_map", vg_1)
+  .then(function(result) {
+
+  })
+  .catch(console.error);
+
+vegaEmbed("#bird_vs_rain", vg_2)
+  .then(function(result) {
+
+  })
+  .catch(console.error);
+
+vegaEmbed("#bird_over_time", vg_3)
+  .then(function(result) {
+
+  })
+  .catch(console.error);
